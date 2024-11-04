eda3af8e6f5e2be75627879f05b097b25d0b0371
Henrique <168787470+Henrique-Tanaka@users.noreply.github.com>
2024-11-04 15:38:50 -0300

    Create Exercício2.java

---
+++ Desafios do curso de Java/Exercício2.java
@@ -0,0 +1,23 @@
+import java.util.Locale;
+import java.util.Scanner;
+public class Main{
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Digite as notas:");
+		double A = sc.nextDouble(), B = sc.nextDouble(), C = (A+B)/2;
+		
+		System.out.printf("Primeira nota: %.1f%n", A);
+		System.out.printf("Segunda nota: %.1f%n", B);
+		System.out.printf("Resultado: %.1f%n", C);
+		
+		if (C > 6) {
+			System.out.println("Aprovado");
+		}
+		else {
+			System.out.println("Reprovado");
+		}
+		
+		sc.close();
+	}
+}
