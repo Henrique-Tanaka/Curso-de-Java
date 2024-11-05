c6146957fb527dc73ed016c846c26a2ebfca3e55
Henrique <168787470+Henrique-Tanaka@users.noreply.github.com>
2024-11-05 16:17:31 -0300

    Create Exercício3.java

---
+++ Desafios do curso de Java/Exercício3.java
@@ -0,0 +1,21 @@
+import java.util.Locale;
+import java.util.Scanner;
+public class Main{
+	public static void main(String[] args){
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Coloca cada número de A, B e C para resolver a fórmula de Bhaskara:");
+		double a = sc.nextDouble(), b = sc.nextDouble(), c = sc.nextDouble();
+		double delta = b*b - 4*a*c;
+		if (a == 0 || delta < 0.0) {
+			System.out.println("Impossível calcular");
+		}
+		else {
+			double x1 = (-b + Math.sqrt(delta))/(2*a), x2 = (-b - Math.sqrt(delta))/(2*a);
+			System.out.printf("%.5f%n", x1);
+			System.out.printf("%.5f",x2);
+		}
+		
+		sc.close();
+	}
+}
