95e9e67ae3975ad80fdfaae2765ccbe1b56f34a7
Henrique <168787470+Henrique-Tanaka@users.noreply.github.com>
2024-11-27 08:45:00 -0300

    Create Exercício7.java

---
+++ Desafios do curso de Java/Exercício7.java
@@ -0,0 +1,25 @@
+//Escreva para cada X e Y uma mensagem que indique se estes valores foram digitados em ordem crescente ou decescente.
+//A leitura deve ser encerrada caso os valores serem iguais.
+import java.util.Locale;
+import java.util.Scanner;
+public class Main {
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		int x = sc.nextInt(), y = sc.nextInt();
+		
+		while (x != y) {
+			if(x < y) {
+				System.out.println("Crescente");
+			}
+			else {
+				System.out.println("Decrescente");
+			}
+			x = sc.nextInt(); 
+			y = sc.nextInt();
+		}
+		
+		sc.close();
+	}
+}
