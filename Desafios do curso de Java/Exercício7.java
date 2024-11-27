391e44b2f4cc10e2e2e6f1c07e42ab88f9d2d3d2
Henrique <168787470+Henrique-Tanaka@users.noreply.github.com>
2024-11-27 09:04:30 -0300

    Update Exercício7.java

--- Desafios do curso de Java/Exercício7.java
+++ Desafios do curso de Java/Exercício7.java
@@ -3,8 +3,7 @@
 import java.util.Scanner;
 public class Main {
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		
+		Scanner sc = new Scanner(System.in);	
 		int x = sc.nextInt(), y = sc.nextInt();
 		
 		while (x != y) {
