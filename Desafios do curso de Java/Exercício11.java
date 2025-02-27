9320afbfd0233798e51be1797ea0f2b779ce2c90
Henrique <168787470+Henrique-Tanaka@users.noreply.github.com>
2025-02-27 17:11:32 -0300

    Update Exercício11.java

--- Desafios do curso de Java/Exercício11.java
+++ Desafios do curso de Java/Exercício11.java
@@ -1,5 +1,5 @@
 //Leia o valor inteiro N(2<N<1000). A seguir, mostre a tabuada de N:
-// 1 x N = 1N     2 x N = 2N  ...  10 x N = 10N
+// 1 x N = 1N; 2 x N = 2N; ... 10 x N = 10N
 //A entrada contém um valor inteiro N(2<N<1000).
 //A saida imprima a tabuada de N.
 import java.util.Scanner;
